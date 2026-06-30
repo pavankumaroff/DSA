b93630a0d0c50f174e7714954799d93cded27a79
Pavan Kumar <pavanmaddala16@gmail.com>
2026-06-30 23:19:07 +0530

    implement bubble sort algorithm with optimization

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-numbers = [0] * 3
+numbers = [1, 5, 9, 3, 2, 6, 7]
+
+
+def sort():
+    for i in range(len(numbers)):
+        for j in range(len(numbers) - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+
+sort()
 
 print(numbers)
--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -3,11 +3,17 @@
 def bubble_sort(array):
     n = len(array)
 
-    for i in range(n):
+    for i in range(n - 1):
+        swapped = False
+
         for j in range(n - i - 1):
             if (array[j] > array[j + 1]):
                 array[j], array[j + 1] = array[j + 1], array[j]
-    
+                swapped = True
+
+        if not swapped:
+            break
+
 
 array = [10, 2, 1, 5, 8, 4, 6, 3, 7, 9]
 bubble_sort(array)
